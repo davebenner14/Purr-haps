fe0afa1832f8f1c5ad0d43ee5048b41949e72c12
David Benner <119465113+davebenner14@users.noreply.github.com>
2024-07-04 20:40:38 -0400

    Fix path to serve static files

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
 
-// Serve static files from the frontend directory
-app.use(express.static(path.join(__dirname, "purr-haps-frontend")));
+// Serve static files from the build directory
+app.use(express.static(path.join(__dirname, "purr-haps-frontend", "build")));
 
 // Catch-all handler to return the index.html file for any requests not handled by the static files
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "purr-haps-frontend", "index.html"));
+  res.sendFile(
+    path.join(__dirname, "purr-haps-frontend", "build", "index.html")
+  );
 });
 
 app.listen(port, "0.0.0.0", () => {
