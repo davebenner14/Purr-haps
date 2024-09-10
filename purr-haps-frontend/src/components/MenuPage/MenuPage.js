756711254d3a9f083ac4a2d683822f29452175ba
David Benner <119465113+davebenner14@users.noreply.github.com>
2024-09-10 10:38:22 -0400

    Update Menu page with new assets

--- purr-haps-frontend/src/components/MenuPage/MenuPage.js
+++ purr-haps-frontend/src/components/MenuPage/MenuPage.js
@@ -2,13 +2,12 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import backgroundImage from "../../assets/MenuPage/Menu Pg Background (1).png";
-import menuSignImage from "../../assets/MenuPage/Menu Sign.png";
+import menuSignImage from "../../assets/MenuSign/PH_Sign.png"; // Updated path
 import signTextImage from "../../assets/MenuPage/Sign Text.png";
 import barTableImage from "../../assets/MenuPage/Bar Table.png";
 import percImage from "../../assets/MenuPage/Perc (Menu Pg) (1).png";
-import buttonImage from "../../assets/MenuPage/Button.png";
-import buttonImage2 from "../../assets/MenuPage/Button2.png";
-import buttonImage3 from "../../assets/MenuPage/Button3.png";
+import buttonImage from "../../assets/MenuSign/PH_SignButtons.png"; // Updated path for normal button state
+import buttonHighlightImage from "../../assets/MenuSign/PH_SignButtons_Highlight.png"; // Path for hover state
 
 import "./MenuPage.css";
 
@@ -30,28 +29,28 @@ const MenuPage = () => {
       <img className="BarTableImage" src={barTableImage} alt="Bar Table" />
       <img className="MenuSignImage" src={menuSignImage} alt="Menu Sign" />
       <img className="SignTextImage" src={signTextImage} alt="Sign Text" />
-      <img className="ButtonImage" src={buttonImage} alt="Button" />
-      <img className="ButtonImage2" src={buttonImage2} alt="Button 2" />
-      <img className="ButtonImage3" src={buttonImage3} alt="Button 3" />
-      <div className="MenuTextContainer">
-        <div
-          className="MenuButtonContainer"
-          onClick={() => handleButtonClick("/menu1")}
-        >
-          <p className="MenuText MenuText1">Character Selection</p>
-        </div>
-        <div
-          className="MenuButtonContainer"
-          onClick={() => handleButtonClick("/bio")}
-        >
-          <p className="MenuText MenuText2">Bio</p>
-        </div>
-        <div
-          className="MenuButtonContainer"
-          onClick={() => handleButtonClick("/contact")}
-        >
-          <p className="MenuText MenuText3">Contact Us</p>
-        </div>
+
+      {/* Each menu button with hover effect */}
+      <div
+        className="MenuButtonContainer"
+        onClick={() => handleButtonClick("/menu1")}
+      >
+        <img className="ButtonImage" src={buttonImage} alt="Button" />
+        <p className="MenuText MenuText1">Character Selection</p>
+      </div>
+      <div
+        className="MenuButtonContainer"
+        onClick={() => handleButtonClick("/bio")}
+      >
+        <img className="ButtonImage" src={buttonImage} alt="Button 2" />
+        <p className="MenuText MenuText2">Bio</p>
+      </div>
+      <div
+        className="MenuButtonContainer"
+        onClick={() => handleButtonClick("/contact")}
+      >
+        <img className="ButtonImage" src={buttonImage} alt="Button 3" />
+        <p className="MenuText MenuText3">Contact Us</p>
       </div>
     </div>
   );
