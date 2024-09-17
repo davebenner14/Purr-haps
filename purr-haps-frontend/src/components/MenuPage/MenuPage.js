d484b431be677497479efd9ab5ffa805d6d8a44e
David Benner <119465113+davebenner14@users.noreply.github.com>
2024-09-17 13:34:31 -0400

    Updated Buttons on Menu page

--- purr-haps-frontend/src/components/MenuPage/MenuPage.js
+++ purr-haps-frontend/src/components/MenuPage/MenuPage.js
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-// Commented out assets for buttons 2-4
-// import backgroundImage from "../../assets/MenuPage/Menu Pg Background (1).png";
-// import menuSignImage from "../../assets/MenuAndButtons/Menu Sign.png";
-// import signTextImage from "../../assets/MenuPage/Sign Text.png";
-// import barTableImage from "../../assets/MenuPage/Bar Table.png";
-// import percImage from "../../assets/MenuPage/Perc (Menu Pg) (1).png";
+// Restore the other assets
+import backgroundImage from "../../assets/MenuPage/Menu Pg Background (1).png";
+import menuSignImage from "../../assets/MenuAndButtons/Menu Sign.png";
+import signTextImage from "../../assets/MenuPage/Sign Text.png";
+import barTableImage from "../../assets/MenuPage/Bar Table.png";
+import percImage from "../../assets/MenuPage/Perc (Menu Pg) (1).png";
 
 import button1Image from "../../assets/MenuAndButtons/Button 1.png";
+// Commented out buttons 2-4 for now
 // import button2Image from "../../assets/MenuAndButtons/Button 2.png";
 // import button3Image from "../../assets/MenuAndButtons/Button 3.png";
 // import button4Image from "../../assets/MenuAndButtons/Button 4.png";
@@ -30,6 +31,17 @@ const MenuPage = () => {
 
   return (
     <div className="PageContainer MenuPage">
+      {/* Restore the background and other assets */}
+      <img
+        className="BackgroundImage"
+        src={backgroundImage}
+        alt="Menu Page Background"
+      />
+      <img className="PercImage" src={percImage} alt="Perc" />
+      <img className="BarTableImage" src={barTableImage} alt="Bar Table" />
+      <img className="MenuSignImage" src={menuSignImage} alt="Menu Sign" />
+      <img className="SignTextImage" src={signTextImage} alt="Sign Text" />
+
       {/* Only focusing on Button 1 */}
       <div
         className="MenuButtonContainer"
