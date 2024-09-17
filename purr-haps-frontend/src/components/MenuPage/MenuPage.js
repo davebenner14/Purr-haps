e60ba31652be0c431f15f56c0c51643560147fe9
David Benner <119465113+davebenner14@users.noreply.github.com>
2024-09-17 13:27:25 -0400

    Updated Buttons on Menu page

--- purr-haps-frontend/src/components/MenuPage/MenuPage.js
+++ purr-haps-frontend/src/components/MenuPage/MenuPage.js
@@ -1,21 +1,22 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-import backgroundImage from "../../assets/MenuPage/Menu Pg Background (1).png";
-import menuSignImage from "../../assets/MenuAndButtons/Menu Sign.png";
-import signTextImage from "../../assets/MenuPage/Sign Text.png";
-import barTableImage from "../../assets/MenuPage/Bar Table.png";
-import percImage from "../../assets/MenuPage/Perc (Menu Pg) (1).png";
+// Commented out assets for buttons 2-4
+// import backgroundImage from "../../assets/MenuPage/Menu Pg Background (1).png";
+// import menuSignImage from "../../assets/MenuAndButtons/Menu Sign.png";
+// import signTextImage from "../../assets/MenuPage/Sign Text.png";
+// import barTableImage from "../../assets/MenuPage/Bar Table.png";
+// import percImage from "../../assets/MenuPage/Perc (Menu Pg) (1).png";
 
 import button1Image from "../../assets/MenuAndButtons/Button 1.png";
-import button2Image from "../../assets/MenuAndButtons/Button 2.png";
-import button3Image from "../../assets/MenuAndButtons/Button 3.png";
-import button4Image from "../../assets/MenuAndButtons/Button 4.png";
+// import button2Image from "../../assets/MenuAndButtons/Button 2.png";
+// import button3Image from "../../assets/MenuAndButtons/Button 3.png";
+// import button4Image from "../../assets/MenuAndButtons/Button 4.png";
 
 import lightUp1Image from "../../assets/MenuAndButtons/Light up 1.png";
-import lightUp2Image from "../../assets/MenuAndButtons/Light up 2.png";
-import lightUp3Image from "../../assets/MenuAndButtons/Light up 3.png";
-import lightUp4Image from "../../assets/MenuAndButtons/Light up 4.png";
+// import lightUp2Image from "../../assets/MenuAndButtons/Light up 2.png";
+// import lightUp3Image from "../../assets/MenuAndButtons/Light up 3.png";
+// import lightUp4Image from "../../assets/MenuAndButtons/Light up 4.png";
 
 import "./MenuPage.css";
 
@@ -23,26 +24,16 @@ const MenuPage = () => {
   const navigate = useNavigate();
   const [hoveredButton, setHoveredButton] = useState(null);
 
-  const handleButtonClick = (path) => {
-    navigate(path);
+  const handleButtonClick = () => {
+    navigate("/menu1");
   };
 
   return (
     <div className="PageContainer MenuPage">
-      <img
-        className="BackgroundImage"
-        src={backgroundImage}
-        alt="Menu Page Background"
-      />
-      <img className="PercImage" src={percImage} alt="Perc" />
-      <img className="BarTableImage" src={barTableImage} alt="Bar Table" />
-      <img className="MenuSignImage" src={menuSignImage} alt="Menu Sign" />
-      <img className="SignTextImage" src={signTextImage} alt="Sign Text" />
-
-      {/* Button 1 */}
+      {/* Only focusing on Button 1 */}
       <div
         className="MenuButtonContainer"
-        onClick={() => handleButtonClick("/menu1")}
+        onClick={handleButtonClick}
         onMouseEnter={() => setHoveredButton(1)}
         onMouseLeave={() => setHoveredButton(null)}
       >
@@ -53,7 +44,8 @@ const MenuPage = () => {
         />
       </div>
 
-      {/* Button 2 */}
+      {/* Commented out buttons 2-4 */}
+      {/*
       <div
         className="MenuButtonContainer"
         onClick={() => handleButtonClick("/bio")}
@@ -67,7 +59,6 @@ const MenuPage = () => {
         />
       </div>
 
-      {/* Button 3 */}
       <div
         className="MenuButtonContainer"
         onClick={() => handleButtonClick("/contact")}
@@ -81,7 +72,6 @@ const MenuPage = () => {
         />
       </div>
 
-      {/* Button 4 */}
       <div
         className="MenuButtonContainer"
         onClick={() => handleButtonClick("/settings")}
@@ -94,6 +84,7 @@ const MenuPage = () => {
           alt="Button 4"
         />
       </div>
+      */}
     </div>
   );
 };
