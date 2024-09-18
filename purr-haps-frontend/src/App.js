14e65a029a882fa5fe4d15174e49d63ff973c516
David Benner <119465113+davebenner14@users.noreply.github.com>
2024-09-17 21:17:52 -0400

    Updated Buttons on Menu page

--- purr-haps-frontend/src/App.js
+++ purr-haps-frontend/src/App.js
@@ -1,9 +1,17 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation
+} from "react-router-dom";
 import "./index.css";
 
 const IntroPage = lazy(() => import("./components/IntroPage/IntroPage"));
-const CharacterSelection = lazy(() => import("./components/CharacterSelect/CharacterSelection"));
+const CharacterSelection = lazy(() =>
+  import("./components/CharacterSelect/CharacterSelection")
+);
 const MenuPage = lazy(() => import("./components/MenuPage/MenuPage"));
 const BioPage = lazy(() => import("./components/BioPage/BioPage"));
 const ContactPage = lazy(() => import("./components/ContactPage/ContactPage"));
@@ -20,13 +28,6 @@ function App() {
   return (
     <Router>
       <Suspense fallback={<div>Loading...</div>}>
-        <nav>
-          <Link to="/" onMouseEnter={() => import('./components/IntroPage/IntroPage')}>Intro Page</Link>
-          <Link to="/menuPage" onMouseEnter={() => import('./components/MenuPage/MenuPage')}>Menu Page</Link>
-          <Link to="/menu1" onMouseEnter={() => import('./components/CharacterSelect/CharacterSelection')}>Character Selection</Link>
-          <Link to="/bio" onMouseEnter={() => import('./components/BioPage/BioPage')}>Bio Page</Link>
-          <Link to="/contact" onMouseEnter={() => import('./components/ContactPage/ContactPage')}>Contact Page</Link>
-        </nav>
         <Routes>
           <Route
             path="/"
