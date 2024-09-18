1a97e6f23eacdbce8ab729c276087e988fd0fa8f
David Benner <119465113+davebenner14@users.noreply.github.com>
2024-09-17 20:31:43 -0400

    Updated Buttons on Menu page

--- purr-haps-frontend/src/components/MenuPage/MenuPage.js
+++ purr-haps-frontend/src/components/MenuPage/MenuPage.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-// Import necessary assets
 import backgroundImage from "../../assets/MenuPage/Menu Pg Background (1).png";
+import menuSignImage from "../../assets/MenuAndButtons/Menu Sign.png";
 import signTextImage from "../../assets/MenuPage/Sign Text.png";
 import barTableImage from "../../assets/MenuPage/Bar Table.png";
 import percImage from "../../assets/MenuPage/Perc (Menu Pg) (1).png";
-import menuSignImage from "../../assets/MenuAndButtons/Menu Sign.png"; // New menu sign
-import button1Image from "../../assets/MenuAndButtons/Button 1.png"; // New button
-import buttonHighlight1Image from "../../assets/MenuAndButtons/Light up 1.png";
+
+import button1Image from "../../assets/MenuAndButtons/Button 1.png";
+import lightUp1Image from "../../assets/MenuAndButtons/Light up 1.png";
 
 import "./MenuPage.css";
 
@@ -17,12 +17,11 @@ const MenuPage = () => {
   const [hoveredButton, setHoveredButton] = useState(false);
 
   const handleButtonClick = (path) => {
-    navigate(path);
+    navigate(path); // Navigate to the next page
   };
 
   return (
     <div className="PageContainer MenuPage">
-      {/* Background and other assets */}
       <img
         className="BackgroundImage"
         src={backgroundImage}
@@ -30,20 +29,24 @@ const MenuPage = () => {
       />
       <img className="PercImage" src={percImage} alt="Perc" />
       <img className="BarTableImage" src={barTableImage} alt="Bar Table" />
-      <img className="SignTextImage" src={signTextImage} alt="Sign Text" />
       <img className="MenuSignImage" src={menuSignImage} alt="Menu Sign" />
+      <img className="SignTextImage" src={signTextImage} alt="Sign Text" />
 
-      {/* Button */}
-      <div
-        className="MenuButtonContainer"
-        onMouseEnter={() => setHoveredButton(true)}
-        onMouseLeave={() => setHoveredButton(false)}
-        onClick={() => handleButtonClick("/menu1")}
-      >
+      {/* Button 1 */}
+      <div className="MenuButtonContainer">
+        {/* This is the button image that changes on hover */}
         <img
           className="ButtonImage"
-          src={hoveredButton ? buttonHighlight1Image : button1Image}
+          src={hoveredButton ? lightUp1Image : button1Image}
           alt="Button 1"
+        />
+
+        {/* Invisible clickable overlay */}
+        <div
+          className="ClickableArea"
+          onClick={() => handleButtonClick("/menu1")}
+          onMouseEnter={() => setHoveredButton(true)} // Set hover state
+          onMouseLeave={() => setHoveredButton(false)} // Remove hover state
         />
       </div>
     </div>
